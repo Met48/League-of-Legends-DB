979f33cfd713327826c2248d11f3b8fb079fa41f
Andrew Sutton <andrew@disqus.com>
2013-11-03 10:38:45 -0800

    Generate champion class via provider.

--- loldb/v2/champion.py
+++ loldb/v2/champion.py
@@ -1,6 +1,5 @@
-"""League of Legend Champion Representation."""
+import re
 
-# TODO: Don't use as many classes?
 
 class Lore(object):
     body = ''
@@ -25,6 +24,7 @@ class Champion(object):
 
     internal_name = ''
     name = ''
+    alias = ''
     title = ''
 
     icon_path = ''
@@ -34,9 +34,8 @@ class Champion(object):
     ratings = None
     tips_as = []
     tips_against = []
+    tags = set()
     abilities = {}
-    # TODO: videos?
-    # TODO: selection sound name?
 
     def __init__(self, internal_name):
         self.internal_name = internal_name
@@ -44,6 +43,7 @@ class Champion(object):
         self.ratings = Ratings()
         self.tips_as = []
         self.tips_against = []
+        self.tags = set()
         self.abilities = {}
 
     def add_tip_as(self, tip):
@@ -61,75 +61,48 @@ class Champion(object):
         return name
 
 
+def _get_tips(tips_str):
+    """Get list of tips from tips string."""
+    return [tip for tip in tips_str.split('*') if tip]
 
-import re
-from .util import get_sql_rows
+
+def _get_description(desc):
+    """Format description text."""
+    # Description often contains '' instead of "
+    return desc.replace("''", '"')
 
 
-def get_champions_map(connection):
-    """Get formatted champion details from sql database."""
-
-    champions_map = {}
-
-    # Process database rows
-    for row in get_sql_rows(connection, 'champions'):
-        # Helper functions
-        def get_tips(string):
-            return [tip for tip in string.split('*') if tip]
-
-        def get_description(string):
-            # The description often contains '' instead of "
-            return string.replace("''", '"')
-
-        # Main data
-        # Stats, skins, and abilities come from other sources
-        champion = {
-            'id': row.id,
-            'internal_name': row.name,
-            'name': row.displayName,
-            'title': row.title,
-            'icon': row.iconPath,
-            'lore': {
-                'desc': get_description(row.description),
-                'quote': row.quote,
-                'quote_author': row.quoteAuthor,
-            },
-            'tips': {
-                'as': get_tips(row.tips),
-                'against': get_tips(row.opponentTips),
-            },
-            'rating': {
-                'attack': row.ratingAttack,
-                'defense': row.ratingDefense,
-                'magic': row.ratingMagic,
-                'difficulty': row.ratingDifficulty,
-            },
-            # TODO: videos?
-            # TODO: selection sound name?
-            'select_sound': row.selectSoundPath,
-        }
-
-        # Tags
-        # A set would be ideal but is not supported by JSON
-        tags = row.tags.split(',')
-        tags_map = dict((tag, True) for tag in tags)
-        champion['tags'] = tags_map
-
+def get_champions(provider):
+    for row in provider.get_db_rows('champions'):
+        # TODO: videos?
+        # TODO: selection sound name?
+        # row.name is the internal name
+        champion = Champion(row.name)
+        # row.displayName is the public name
+        champion.name = row.displayName
+        # Misc
+        champion.title = row.title
+        champion.id = row.id
+        champion.icon_path = row.iconPath
+        champion.tags = set(row.tags.split(','))
+        # Lore
+        champion.lore.body = _get_description(row.description)
+        champion.lore.quote = row.quote
+        champion.lore.quote_author = row.quoteAuthor
+        # Ratings
+        champion.ratings.attack = row.ratingAttack
+        champion.ratings.defense = row.ratingDefense
+        champion.ratings.magic = row.ratingMagic
+        champion.ratings.difficulty = row.ratingDifficulty
+        # Tips
+        tips_as = _get_tips(row.tips)
+        tips_against = _get_tips(row.opponentTips)
+        map(champion.add_tip_as, tips_as)
+        map(champion.add_tip_against, tips_against)
         # For convenience, champions should be accessible via ascii variable
         # names; any groups of invalid characters will be replaced by _
-        name = row.displayName.lower()
-        name = re.sub('[^a-z0-9]+', '_', name)
-        # name = re.sub('^[0-9]', '', name)
-        champion['aliases'] = [name]
+        alias = row.displayName.lower()
+        alias = re.sub('[^a-z0-9]+', '_', alias)
+        champion.alias = alias
 
-        # Store by id
-        champions_map[row.id] = champion
-
-    return champions_map
-
-
-def correct_champions_map(champions_map, corrections_map):
-    """Correct champions in-place."""
-
-    # Currently no corrections are needed for champions
-    return
+        yield champion
