49fe76bb1159e285e2eb054987287bfa5cfea26a
Andrew Sutton <andrew@disqus.com>
2013-11-03 16:59:13 -0800

    WIP champion.py refactor.

--- loldb/v2/champion.py
+++ loldb/v2/champion.py
@@ -1,5 +1,5 @@
 import collections
-import re
+import warnings
 
 from inibin import Inibin
 
@@ -32,7 +32,6 @@ class Lore(object):
         self.body = self._correct_description(row.description)
         self.quote = row.quote
         self.quote_author = row.quoteAuthor
-
 
 
 class Ratings(object):
@@ -122,71 +121,117 @@ class Champion(object):
         return '<Champion {} \'{}\'>'.format(self.id, self.name)
 
 
-def _get_tips(tips_str):
-    """Get list of tips from tips string."""
-    return [tip.strip() for tip in tips_str.split('*') if tip]
-
-
 def _find_inibin(provider, pattern):
     raf_master = provider.get_raf_master()
     raf_results = list(raf_master.find_re(pattern))
     if not raf_results:
-        print('No inibin for %s' % pattern)
+        warnings.warn('No inibin for %s' % pattern)
     if len(raf_results) > 1:
         # TODO: Is this ever triggered?
-        print('Ambiguous inibin for %s' % pattern)
+        warnings.warn('Ambiguous inibin for %s' % pattern)
     try:
         inibin = Inibin(data=raf_results[0].read())
-    except Exception:  # TODO: Less generic Exception
-        print('Malformed inibin for %s' % pattern)
-        return None
-    else:
-        return inibin
+    except Exception:
+        warnings.warn('Malformed inibin for %s' % pattern)
+        inibin = None
+    return inibin
+
+
+def _get_tips_from_string(tips_str):
+    """Get list of tips from tips string."""
+    return [tip.strip() for tip in tips_str.split('*') if tip]
+
+
+def _get_raw_champion_from_provider_and_sql_row(provider, row):
+    """
+    Create a Champion using a row from the champions table in the database.
+
+    Champion will be incomplete as stats and abilities are only available
+     using inibins;
+
+    """
+    # TODO: videos? selection sound name?
+    # row.name is the internal name
+    champion = Champion(row.name)
+    # row.displayName is the public name
+    champion.name = row.displayName
+
+    # Misc
+    champion.alias = alias(row.displayName)
+    champion.title = row.title
+    champion.id = row.id
+    champion.icon_path = row.iconPath
+    champion.tags = set(row.tags.split(','))
+    champion.lore.update_from_sql_row(row)
+    champion.ratings.update_from_sql_row(row)
+
+    # Tips
+    champion.tips_as = _get_tips_from_string(row.tips)
+    champion.tips_against = _get_tips_from_string(row.opponentTips)
+
+    _update_raw_champion_with_provider(champion, provider)
+
+    return champion
+
+
+def _update_raw_champion_with_provider(champion, provider):
+    """Update champion stats and abilities."""
+    # Find champion inibin
+    champ_name = champion.internal_name
+    champ_pattern = '^data/characters/{0}/{0}.inibin$'.format(champ_name)
+    champ_inibin = _find_inibin(provider, champ_pattern)
+
+    if champ_inibin is None:
+        warnings.warn('Missing inibin for champion %s' % champ_name)
+        return
+
+    # Format as champion inibin
+    font_config = provider.get_font_config()
+    champ_inibin = champ_inibin.as_champion(font_config)
+
+    # Read stats
+    champion.stats.update_from_inibin(champ_inibin)
+
+    # Find abilities
+    _update_champion_abilities(provider, champion, champ_inibin['abilities'])
+
+
+_ABILITY_KEYS = list('skill%d' % i for i in range(1, 5))
+
+
+def _update_champion_abilities(provider, champion, abilities):
+    abilities = [abilities[key] for key in _ABILITY_KEYS]
+    for i, ability_name in enumerate(abilities):
+        # Find inibin for ability
+        ability_inibin = _find_ability_inibin(provider, champion, ability_name)
+        if ability_inibin is None:
+            continue
+
+        # Format as ability inibin
+        font_config = provider.get_font_config()
+        ability_inibin = ability_inibin.as_ability(font_config)
+
+        ability = Ability.from_inibin(ability_inibin, i)
+
+        if ability is not None:
+            champion.abilities.append(ability)
+
+
+_ABILITY_REGEXP_TEMPLATE = r"^data/(?:characters/{0}/)?spells/{1}.inibin$"
+
+
+def _find_ability_inibin(provider, champion, ability_name):
+    # Find ability inibin
+    champ_name = champion.internal_name
+    ability_pattern = _ABILITY_REGEXP_TEMPLATE.format(champ_name, ability_name)
+    ability_inibin = _find_inibin(provider, ability_pattern)
+
+    if ability_inibin is None:
+        warnings.warn('Missing inibin for ability %s' % ability_name)
+
+    return ability_inibin
 
 
 def get_champions(provider):
     for row in provider.get_db_rows('champions'):
-        # TODO: videos?
-        # TODO: selection sound name?
-        # row.name is the internal name
-        champion = Champion(row.name)
-        # row.displayName is the public name
-        champion.name = row.displayName
-
-        # Misc
-        champion.alias = alias(row.displayName)
-        champion.title = row.title
-        champion.id = row.id
-        champion.icon_path = row.iconPath
-        champion.tags = set(row.tags.split(','))
-        champion.lore.update_from_sql_row(row)
-        champion.ratings.update_from_sql_row(row)
-
-        # Tips
-        champion.tips_as = _get_tips(row.tips)
-        champion.tips_against = _get_tips(row.opponentTips)
-
-        # Find champion inibin
-        champ_name = champion.internal_name.lower()
-        champ_pattern = '^data/characters/{0}/{0}.inibin$'.format(champ_name)
-        champ_inibin = _find_inibin(provider, champ_pattern)
-        if champ_inibin is not None:
-            font_config = provider.get_font_config()
-            champ_inibin = champ_inibin.as_champion(font_config)
-            # Read stats
-            champion.stats.update_from_inibin(champ_inibin)
-
-            # Find abilities
-            abilities = champ_inibin['abilities']
-            ability_keys = ['skill%d' % i for i in range(1, 5)]
-            abilities = [abilities[key] for key in ability_keys]
-            for i, ability_name in enumerate(abilities):
-                ability_pattern = r"^data/(?:characters/{0}/)?spells/{1}.inibin$".format(champ_name, ability_name)
-                ability_inibin = _find_inibin(provider, ability_pattern)
-                if ability_inibin is None:
-                    continue
-                ability_inibin = ability_inibin.as_ability(font_config)
-                ability = Ability.from_inibin(ability_inibin, i)
-                champion.abilities.append(ability)
-
-        yield champion
+        yield _get_raw_champion_from_provider_and_sql_row(provider, row)
