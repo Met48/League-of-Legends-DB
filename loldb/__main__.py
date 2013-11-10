2814f1df4dce293ae3b96fe54a12024a77fb4fb9
Andrew Sutton <me@andrewcsutton.com>
2013-11-09 17:14:41 -0800

    Exit on path error. Create directories for output.

--- loldb/__main__.py
+++ loldb/__main__.py
@@ -44,23 +44,6 @@ if sys.version[0] == '3':
     raw_input = input
 
 
-def get_answer(question, args):
-    if args['--force']:
-        return True
-    if args['--no']:
-        return False
-
-    print(question)
-
-    while True:
-        result = raw_input('Y/N > ').lower()
-        if result in ('y', 'yes'):
-            return True
-        if result in ('n', 'no'):
-            return False
-        print('Answer Y/N')
-
-
 def main(args):
     if not args['stats']:
         return
@@ -69,13 +52,13 @@ def main(args):
     path = args['--path']
     if not os.path.isdir(path):
         print('Invalid directory "%s"' % path)
+        exit(1)
     if not os.path.isdir(os.path.join(path, 'RADS')):
-        print('Not a valid LoL installation, there should be a RADS folder present.')
+        print('Not a valid LoL installation, path should contain RADS folder.')
+        exit(1)
 
     if not args['--json'] and not args['--yaml']:
-        if not get_answer('No output format specified, continue?', args):
-            print('Aborting.')
-            return
+        ask_about_warning('No output format specified, continue?', args)
 
     provider = ResourceProvider(
         lol_path=path,
@@ -91,22 +74,66 @@ def main(args):
         print('\n'.join(validation_errors))
         print('%d validation errors.' % len(validation_errors))
         if validation_errors:
-            if not get_answer('Validation errors encountered, continue?', args):
-                print('Aborting.')
-                return
+            ask_about_warning('Validation errors encountered, continue?', args)
 
     champions = [format_champion(champion) for champion in champions]
 
     items = get_items(provider)
     items = dict((key, format_item(item)) for key, item in items.items())
 
-    if args['--json']:
-        with open(args['--json'], 'w') as f:
+    json_path = args['--json']
+    if json_path:
+        prepare_write_path(json_path)
+        with open(json_path, 'w') as f:
             f.write(to_json(champions, items))
 
-    if args['--yaml']:
-        with open(args['--yaml'], 'w') as f:
+    yaml_path = args['--yaml']
+    if yaml_path:
+        prepare_write_path(yaml_path)
+        with open(yaml_path, 'w') as f:
             f.write(to_yaml(champions, items))
+
+
+def ask_about_warning(warning, args):
+    """
+    Ask the user if warning is acceptable. If not, exit.
+
+    Will respect options that force a response.
+    """
+    print(warning)
+    if args['--no']:
+        result = False
+    elif args['--force']:
+        result = True
+    else:
+        result = ask_yes_no()
+
+    if not result:
+        print('Aborting')
+        exit(1)
+
+
+def ask_yes_no():
+    """Get a yes or no response from the user."""
+    while True:
+        result = raw_input('Y/N > ').lower()
+        if result in ('y', 'yes'):
+            return True
+        if result in ('n', 'no'):
+            return False
+        print('Answer Y/N')
+
+
+def prepare_write_path(path):
+    """
+    Create missing directories such that path may be opened.
+
+    May raise an exception as it ignores potential race conditions.
+    """
+    directory = os.path.dirname(path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
 
 if __name__ == '__main__':
     main(docopt.docopt(__doc__, version='LoLDB v%s' % __version__))
